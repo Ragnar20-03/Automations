aff51076a22e801e9b4fd392d0cdc4fb6e31088d
Ragnar20-03 <roshanpp20@gmail.com>
2023-10-29 15:17:23 +0530

    Added Time

--- FileAutomation/__new/HashFile.py
+++ FileAutomation/__new/HashFile.py
@@ -4,6 +4,7 @@
 import os 
 from sys import * 
 import hashlib
+import time 
 
 # ////////////////////////////////////////////
 #   
@@ -71,16 +72,17 @@ def hashfile(path , blocksize = 4096):
 # 
 # ////////////////////////////////////////////
 
-def travelDirectory(path):
+def findDuplicates(path):
     exists = os.path.isdir(path)
     dups = {}
     if exists :
+        print("Scanning Files ......")
         for dirName  , subdirs  , fileList in os.walk(path):
             # print("Current Folder is : " , dirName , "***********")
             for file in fileList :
                 path = os.path.join(dirName , file)
+                print("File Name : : \t\t" , path)
                 file_hash = hashfile(path)
-
                 if file_hash in dups :
                     dups[file_hash].append(path)
                 else : 
@@ -102,24 +104,59 @@ def travelDirectory(path):
 # ////////////////////////////////////////////
 
 def printDuplicate (dict1):
+    iSize = 0 
+    iCount = 0 
     dulplicateList = list(filter(lambda x : len(x) > 1 , dict1.values()))
     if(len(dulplicateList) > 0):
         print("Duplcaite File Are  ")
         for result in dulplicateList:
             for subresult in result : 
+                iSize = iSize + os.path.getsize(subresult)
+                iCount = 1 +  iCount
                 print(subresult)
 
+    return iSize,iCount
 # ////////////////////////////////////////////
 
 
 # Starter Function
 
 def main():
-    print("*"*10)
-    dict1 = travelDirectory(argv[1])
-    printDuplicate(dict1)
-    print("\n")
-    removeDuplicates(dict1)
+    print ("\n\n#################################################################")
+    print("------ Duplicate File Removal -----  ")
+    print ("################################################################# \n\n")
+
+    if len(argv)!= 2 :
+        print("Invalid Number of Arguments : ")
+        exit()
+    
+    if (argv[1] == '-h') or (argv[1] == '-H'):
+        print("This Script is used to Detect Dulicate Files And remove them From System..")
+    
+    if (argv[1] == '-u') or (argv[1] == '-U'):
+        print("Uasge : ApllicationName Abs_Path_of_Directory")
+        exit()
+
+    try: 
+        dict1 = {}
+        startTime = time.time()   
+        dict1 = findDuplicates(argv[1])
+        iSize , iCount = printDuplicate(dict1)
+        print("\n")
+        print("Duplicate Files Count is : " , iCount)
+        print("Duplicate Files Data Size : " ,( iSize /1024/1024/1024), "GB")
+        print("Duplicate Files Data Size : " ,( iSize /1024/1024), "MB")
+
+        print("\n")
+        removeDuplicates(dict1)
+        endTime = time.time()
+
+        print("\n\nTook %s seconds to execute ... \n\n" %(endTime - startTime))
+    
+    except ValueError:
+        print("Invalid DataType of input : ")
+    except Exception as E :
+        print("Unknown Error Occured : " , E)
 
 # Stater Function call:
 if __name__ == "__main__":
